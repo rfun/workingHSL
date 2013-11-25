6024cc31ce3cdc05f32b0d3c361a5e23061b3bb1
Rohit Khattar <rohit@byu.net>
2013-11-25 01:56:00 -0700

    Fixed NoDataValue Error

--- db_get_data2.php
+++ db_get_data2.php
@@ -7,6 +7,12 @@ $startdate=$_GET['startdate'];
 $enddate=$_GET['enddate'];
 $methodid=$_GET['meth'];
 
+$query2 = "SELECT NoDataValue FROM variables";
+$query2 .= " WHERE VariableID=".$varid;
+$result2 = mysql_query($query2) or die("SQL Error 1: " . mysql_error());
+$unitid = mysql_fetch_array($result2, MYSQL_ASSOC);
+$NoValue = $unitid['NoDataValue'];
+
 $query = "SELECT ValueID, DataValue, LocalDateTime FROM datavalues";
 $query .= " WHERE SiteID=".$siteid." and VariableID=".$varid." and MethodID='$methodid' and LocalDateTime between '".$startdate."' and '".$enddate."' ORDER BY LocalDateTime ASC";
 
@@ -16,12 +22,14 @@ $num_rows = mysql_num_rows($result);
 $count=1;
  while ($row = mysql_fetch_array($result, MYSQL_ASSOC))
   {
+	  if (!($row['DataValue'] == $NoValue))
+{
     echocsv( $row );
    if($count!=$num_rows)
 	{echo "\r\n";}
   $count=$count+1;
   }
-
+  }
 
   function echocsv( $fields )
   {
